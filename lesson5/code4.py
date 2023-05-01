8e41f1f82376fe4969990521b16862fb27908857
Amankaium <kaium@list.ru>
2023-05-01 21:24:05 +0600

    added lesson5

--- lesson5/code4.py
+++ lesson5/code4.py
@@ -1,3 +1,22 @@
-from my_min import mad_min, my_sort
-print(mad_min([7, 4]))
-# print(__name__)
+from my_min import *
+# print(mad_min([7, 4]), "2 строка code4.py")
+# print(__name__, "3 строка code4.py")
+
+my_array = [6, 8, 3, 6, 9]
+# print(my_sort(my_array))
+# print(my_sort(list("hello")))
+
+# print(factorial(1000))
+# print(recursion_factorial(1000))
+
+big_lst = [
+    7,
+    "example",
+    False,
+    None,
+    [7, 6, 23, 6],
+    [[[[[[37, 7, 332, [[[[[373, 52]]]]]]]]]]],
+    82
+]
+
+print(cherdak(big_lst))
--- lesson5/my_min.py
+++ lesson5/my_min.py
@@ -7,11 +7,39 @@ def mad_min(lst):
             my_min = num
     return my_min
 
-def my_sort():
-    return 123
+# функция сортировки списка
+def my_sort(array):
+    sorted_array = []
+    for i in range(len(array)):
+        new_min = mad_min(array) # 3
+        sorted_array.append(new_min) # [3]
+        array.remove(new_min) # [6, 8, 9]
+    return sorted_array
 
-# print(__name__)
+# функция факториала числа
+def factorial(num): # 5
+    result = 1
+    for i in range(1, num + 1):  # 1, 2, 3, 4, 5
+        result = result * i # 24 * 5
+    return result
+
+def recursion_factorial(num):
+    if num == 1:
+        return 1
+    return recursion_factorial(num-1) * num
+
+
+def cherdak(var, result=[]):
+    if not isinstance(var, list):
+        result.append(var)
+        return result  # [7]
+    else:
+        for element in var:
+            result = cherdak(element, result)  # 1 - cherdak(7, [])  2 -  cherdak("example", [7])
+        return result
+
+
+# print(__name__, "13 строка my_min.py")
 if __name__ == "__main__":
     my_lst = [0, -2, 3, -6]
     print(mad_min(my_lst))
-
